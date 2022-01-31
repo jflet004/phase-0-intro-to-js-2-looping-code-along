fb4fcfb3f3c2ed25b412db2279b07aaf5ed1dca3
Jose Fletes <joseangelfletes@gmail.com>
2022-01-30 19:12:02 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCards(names, eventName) {
+    const messages = [];
+
+    for (let i = 0; i < names.length; i++) {
+       messages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+    }
+ 
+    return messages;
+}
+writeCards(["Jose", "Cassandra", "Batboy", "Luigi"], "birthday");
+
+function countDown(int) {
+    while (int >= 0) {
+        console.log(int--);
+    }
+}
+//console.log(countDown(20));
